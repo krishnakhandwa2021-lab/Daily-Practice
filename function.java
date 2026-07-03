5c2c77ca950b104e6f6e6454ff7f0226340af532
krishna008 <krishna.khandwa2021@gmail.com>
2026-07-03 11:18:06 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ public class function {
+ 
+    public static void printhelloworld(){ 
+        System.out.println(" Hello world");
+    }
+
+    public static void main(String[] args) {
+        printhelloworld();
+    }
+ }
---
+++ parameters.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class parameters {
+    public static void calculateSum(int num1,int num2){ 
+        int sum = num1 +num2;
+        System.out.println("sum is : " + sum );
+    }
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+       calculateSum(a, b);
+    
+}
+}
