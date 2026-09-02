035faf34ecf53cf7566df1540b0c0df74a0e63ca
krishna008 <krishna.khandwa2021@gmail.com>
2026-09-03 01:13:06 +0530

    Add files via upload

---
+++ calculatesum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class calculatesum {
+    public static void calculatesum () {
+        Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int sum = a + b ; 
+         System.out.println(" sum is = " + sum );
+        
+    }
+
+     public static void main(String[] args) {
+         calculatesum();
+
+     }
+}
